78739d234006e60150fde2dc8d5c86568cb5e9a2
ThermalEng <38897886+ThermalEng@users.noreply.github.com>
2025-05-23 06:47:52 +0800

    Update diy.sh
    防止版本校验的错误

--- diy.sh
+++ diy.sh
@@ -3,8 +3,9 @@
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 ./scripts/feeds update -a
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,luci-lib-taskd}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 rm -rf feeds/packages/net/mosdns && cp -rf ./feeds/smpackage/mosdns ./feeds/packages/net/mosdns
+curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/4a9fafefd67172e074fa62cbe3570c4e197376b3/openwrt/patch/apk-tools/9999-hack-for-linux-pre-releases.patch > package/system/apk/patches/9999-hack-for-linux-pre-releases.patch
 ./scripts/feeds install -a
 cp -f ../.config .config
 make defconfig
